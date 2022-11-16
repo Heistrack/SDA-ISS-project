83bc3c5bc3a759bbce701780ba515eaab531e366
Heistrack <kuba8070@gmail.com>
2022-11-16 20:29:12 +0100

    feature(wra): connection with station loc API with dependency added

--- src/main/java/pl/sda/Connection.java
+++ src/main/java/pl/sda/Connection.java
@@ -1,12 +1,64 @@
 package pl.sda;
 
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpRequestBase;
+import org.apache.http.client.utils.URIBuilder;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Connection {
 
+    public JSONObject getStaLoc() {
+
+        try {
+
+            URL url = new URL("http://api.open-notify.org/iss-now.json");
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+
+            int responseCode = conn.getResponseCode();
+
+            if (responseCode != 200) {
+                throw new RuntimeException("HttpResponseCode: " + responseCode);
+            } else {
+                StringBuilder informationString = new StringBuilder();
+                Scanner scanner = new Scanner(url.openStream());
+
+                while (scanner.hasNext()) {
+                    informationString.append(scanner.nextLine());
+                }
+
+                scanner.close();
+
+                JSONParser parse = new JSONParser();
+                Object dataObject = parse.parse(String.valueOf(informationString));
+
+                System.out.println(dataObject);
+
+                JSONObject stationLoc = (JSONObject) dataObject;
+
+                return stationLoc;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 
 }
--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -3,8 +3,10 @@ package pl.sda;
 
 public class Main {
     public static void main(String[] args) {
-        GUI gui = new GUI();
-        System.out.println("hello");
-        gui.showGUI();
+        Connection connection = new Connection();
+        connection.getStaLoc();
+//        GUI gui = new GUI();
+//        System.out.println("hello");
+//        gui.showGUI();
     }
 }
